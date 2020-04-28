97b404977d5b2a6c849c82c073a1f7e11d6b5f76
zijonas <jonas.ziel@terra.com.br>
2020-04-28 21:54:12 +0200

    Reorganizing Project

--- src/app/utils/category.pipe.spec.ts
+++
@@ -1,9 +0,0 @@
-import { CategoryPipe } from './category.pipe';
-import { CategoryService } from '../services/category.service';
-
-describe('CategoriePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CategoryPipe(new CategoryService(null));
-    expect(pipe).toBeTruthy();
-  });
-});
